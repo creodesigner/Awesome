af42ab1fee03d0c892a45b22e22c77fd4a72a032
Jace Harmer <jaceharmer@gmail.com>
2016-08-10 20:13:25 -0600

    more.

--- functions.js
+++ functions.js
@@ -1,9 +1,14 @@
 (function ($) {
   var anything = {
     formSubmit: function(e){
+      var url = 'https://spreadsheets.google.com/feeds/list/1DD7CJGb7Hd0YcvgYP7TFjUD35WYYjrG9iaf2CQOcSgI/';
       e.preventDefault();
 
-      alert('Form submitted yo!!!!!! Brosky.');
+      $.post(url, $('#form').serialize(), function(){
+        alert('Success!');
+      }).fail(function(data){
+        alert('FAILED. ' + data.statusText);
+      });
     }
   };
 
